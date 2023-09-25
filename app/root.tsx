fdd467c4e1b926520b2677cee4a20bcef63d6f62
Dirk Metzler <dirkmetzler@icloud.com>
2023-09-25 18:54:10 +0200

    Add and run prettier

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./globals.css"
+import "./globals.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
