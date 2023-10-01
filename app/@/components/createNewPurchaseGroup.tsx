9c5d2c1586d00d0dff016615f44afe353f577e09
Dirk Metzler <dirkmetzler@icloud.com>
2023-10-01 13:03:05 +0200

    Move new purchase group to the dialog

--- app/@/components/createNewPurchaseGroup.tsx
+++ app/@/components/createNewPurchaseGroup.tsx
@@ -2,30 +2,55 @@ import { Form } from "@remix-run/react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "app/@/components/ui/dialog";
+import { Plus } from "lucide-react";
 
 export default function CreateNewPurchaseGroup() {
   return (
-    <Form
-      method="POST"
-      className="flex flex-col justify-center items-center w-full max-w-xl"
-    >
-      <div className="flex flex-col justify-center w-full p-4 gap-6 rounded-md">
-        <section className="flex flex-col gap-3">
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" name="name" placeholder="Name" />
-        </section>
-        <section className="flex flex-col gap-3">
-          <Label htmlFor="description">Description</Label>
-          <Input type="text" name="description" placeholder="Description" />
-        </section>
-        <section className="flex flex-col gap-3">
-          <Label htmlFor="emrecipientsail">Recipients</Label>
-          <Input type="text" name="recipients" placeholder="recipients" />
-        </section>
-        <section>
-          <Button type="submit">Create</Button>{" "}
-        </section>
-      </div>
-    </Form>
+    <section className="p-4">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">
+            <Plus className="mr-3" />
+            new purchase group
+          </Button>
+        </DialogTrigger>
+        <Form
+          method="POST"
+          className="flex flex-col justify-center items-center w-full max-w-xl"
+        >
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogDescription>
+                Create your new purchase group.
+              </DialogDescription>
+            </DialogHeader>
+            <section className="flex flex-col gap-3">
+              <Label htmlFor="name">Name</Label>
+              <Input type="text" name="name" placeholder="Name" />
+            </section>
+            <section className="flex flex-col gap-3">
+              <Label htmlFor="description">Description</Label>
+              <Input type="text" name="description" placeholder="Description" />
+            </section>
+            <section className="flex flex-col gap-3">
+              <Label htmlFor="emrecipientsail">Recipients</Label>
+              <Input type="text" name="recipients" placeholder="recipients" />
+            </section>
+            <DialogFooter>
+              <Button type="submit">Create</Button>
+            </DialogFooter>
+          </DialogContent>{" "}
+        </Form>
+      </Dialog>
+    </section>
   );
 }
